9e85baa0f0ba994e33ac80212ef8aaa40d1c5953
iness22 <ines.bouguerra@esprit.tn>
2019-10-29 19:53:55 +0100

    gestion livraison page d'accueil

---
+++ GestionLivraisonController.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author ines bouguerra
+ */
+public class GestionLivraisonController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void manageDelivery(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AjouterLivraison.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            ((Node) event.getSource()).getScene().getWindow().hide();
+            Stage stage = new Stage();
+            stage.setTitle("Gestion Livraison Compagny");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ex) {
+            System.out.println("Cant load a new window");
+        
+    }
+    }
+
+    @FXML
+    private void listGelivery(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ListeLivraison.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            ((Node) event.getSource()).getScene().getWindow().hide();
+            Stage stage = new Stage();
+            stage.setTitle("deliveru history");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception ex) {
+            System.out.println("Cant load a new window");
+        
+    }
+    }
+
+    @FXML
+    private void mapDelivery(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MapFXML.fxml"));
+            Parent root = (Parent) fxmlLoader.load();
+            ((Node) event.getSource()).getScene().getWindow().hide();
+            Stage stage = new Stage();
+            stage.setTitle("follow delivery");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception ex) {
+            System.out.println("Cant load a new window");
+        
+    }
+    }
+
+    @FXML
+    private void backHomePage(ActionEvent event) {
+    }
+    
+}
