362a48db061df1c381ffab326b722587595d6ff6
iness22 <ines.bouguerra@esprit.tn>
2019-10-29 19:57:26 +0100

    list of deliveries with some fonctionalities

--- ListeLivraisonController.java
+++ ListeLivraisonController.java
@@ -56,42 +56,42 @@ public class ListeLivraisonController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         List<Delivery> deliveries = new ArrayList<Delivery>();
         ServiceDelivery serviceDelivery = new ServiceDelivery();
-        
+
         try {
             //System.out.println(serviceDelivery.readAll().size());
             //deliveries = serviceDelivery.readAll();
             //nombrepromotion.setText(String.valueOf(promotions.size()));
 
             /////*******//////
-            deliveries=serviceDelivery.readAllDelivery();
+            deliveries = serviceDelivery.readAllDelivery();
             deliveriesData.addAll(deliveries);
         } catch (SQLException ex) {
             Logger.getLogger(ListeLivraisonController.class.getName()).log(Level.SEVERE, null, ex);
         }
-            cid.setCellValueFactory(new PropertyValueFactory<>("IdDelivery"));
-            cdate.setCellValueFactory(new PropertyValueFactory<>("DateDelivery"));
-            cdiscription.setCellValueFactory(new PropertyValueFactory<>("DescriptionDelivery"));
-            cstate.setCellValueFactory(new PropertyValueFactory<>("StateDelivery"));
-            listelivraison.setItems(deliveriesData);
-            
+        cid.setCellValueFactory(new PropertyValueFactory<>("IdDelivery"));
+        cdate.setCellValueFactory(new PropertyValueFactory<>("DateDelivery"));
+        cdiscription.setCellValueFactory(new PropertyValueFactory<>("DescriptionDelivery"));
+        cstate.setCellValueFactory(new PropertyValueFactory<>("StateDelivery"));
+        listelivraison.setItems(deliveriesData);
+
     }
-            
-        
 
     @FXML
     private void afficheLivraison(MouseEvent event) {
+        
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ModifierLivraison.fxml"));
             Parent root1 = (Parent) fxmlLoader.load();
             ((Node) event.getSource()).getScene().getWindow().hide();
             Stage stage = new Stage();
-            stage.setTitle("ListePromotion");
             stage.setScene(new Scene(root1));
             stage.show();
+
         } catch (Exception ex) {
             System.out.println("Cant load a new window");
 
         }
+
     }
 
     @FXML
@@ -109,5 +109,5 @@ public class ListeLivraisonController implements Initializable {
 
         }
     }
-    
+
 }
